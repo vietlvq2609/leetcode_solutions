f8f4694b183cf4168245b7f1376f29bbc6293dea
vietlvq2609 <vietlvq2609@gmail.com>
2024-12-27 16:54:38 +0700

    update solution

---
+++ python/160IntersectionOfTwoLinkedLists.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        aSet = set()
+        bSet = set()
+        while headA and headB:
+            aSet.add(headA)
+            bSet.add(headB)
+            if headB in aSet:
+                return headB
+            if headA in bSet:
+                return headA
+            headA = headA.next
+            headB = headB.next
+        while headA:
+            if headA in bSet:
+                return headA
+            headA = headA.next
+        while headB:
+            if headB in aSet:
+                return headB
+            headB = headB.next
+        return None
+
+    # More efficient solution
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
+        pointerA, pointerB = headA, headB
+        while pointerA != pointerB:
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+        return pointerA
