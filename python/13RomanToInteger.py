71027c4d998a243974d0589b0988f92f1fc1439e
vietlvq2609 <vietlvq2609@gmail.com>
2025-01-08 12:36:14 +0700

    13 - Roman to Integer - python updated solution

--- python/13RomanToInteger.py
+++ python/13RomanToInteger.py
@@ -1,31 +1,23 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        values = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
         total = 0
-        for i in range(0, len(s)):
-            if i+1 == len(s):
-                total += self.intFromRoman(s[i])
-                break
-            if self.intFromRoman(s[i+1]) > self.intFromRoman(s[i]):
-                total -= self.intFromRoman(s[i])
+        for i in range(len(s) - 1):
+            if values[s[i]] < values[s[i + 1]]:
+                total -= values[s[i]]
             else:
-                total += self.intFromRoman(s[i])
+                total += values[s[i]]
+        total += values[s[-1]]
         return total
 
-
-
-    def intFromRoman(self, r: str) -> int:
-        if r == 'I':
-            return 1
-        if r == 'V':
-            return 5
-        if r == 'X':
-            return 10
-        if r == 'L':
-            return 50
-        if r == 'C':
-            return 100
-        if r == 'D':
-            return 500
-        if r == 'M':
-            return 1000
-        return -1
+solution = Solution()
+s = "III"
+print(solution.romanToInt(s)) # 3
