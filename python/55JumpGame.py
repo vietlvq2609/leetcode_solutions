cc5cd7176c7d7e48da28d511faba0b34c7d34a9d
vietlvq2609 <vietlvq2609@gmail.com>
2025-01-08 11:45:09 +0700

    55 - Jump Game - python

---
+++ python/55JumpGame.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    # Gready approach
+    def canJump(self, nums: List[int]) -> bool:
+        max_reachable = 0
+        for i in range(len(nums)):
+            if i > max_reachable:
+                return False
+            max_reachable = max(max_reachable, i + nums[i])
+            if max_reachable >= len(nums) - 1:
+                return True
+        return False
+
+    # Dynamic programming approach
+    def canJumpDP(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False] * n
+        dp[0] = True
+        for i in range(n):
+            if dp[i]:
+                for j in range(i + 1, min(i + nums[i] + 1, n)):
+                    dp[j] = True
+        return dp[n - 1]
+
+solution = Solution()
+nums = [3,2,1,0,4]
+print(solution.canJump(nums)) # True
